85ab9497b76e5bfa043313dd42ceb44b2a46ebc8
ramope <gataraki@gmail.com>
2019-01-13 11:01:12 +0100

    module1-sol

--- app.js
+++ app.js
@@ -4,11 +4,28 @@
 	.controller('LunchCheckController',LunchCheckController)
 	LunchCheckController.$inject=['$scope'];
 	function LunchCheckController($scope){
-		$scope.CheckIfTooMuch(){
- $scope.name = "Raquel";
- $scope.stateOfBeing = "fed";
- console.log('hola');
+        $scope.count=0;
+        $scope.lunchmenu='';
+        $scope.myStyle={"color":"black"};
+       
+		$scope.CheckIfTooMuch=function(){
+        $scope.count++;
+        var str=$scope.lunchmenu;
+        var arrayOfStrings=str.split(',');
+        var le=arrayOfStrings.length;
+       /* console.log($scope.lunchmenu);*/
+        if(($scope.count==1)&&($scope.lunchmenu=='')){
+            $scope.msg='Please enter data first';}
+         else if(le<=3){
+         	$scope.myStyle={"color":"green","font-size" : "50px"};
+         	$scope.msg='Enjoy!';}
+
+         else {
+         	$scope.myStyle={"color":"red","font-size" : "40px"};
+         	$scope.msg='Too much!';}
+           
 		};
+
 
 	}
 
